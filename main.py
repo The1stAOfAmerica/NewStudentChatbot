9af6585d8389b5b17f9988835331c1da63016556
amitkrishna <akrocks1248@gmail.com>
2023-08-15 11:36:22 -0700

    push 1 wowza

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello New Student! What would you like to know about?\n")
+answer = input("Here are some suggested Questions:\n"
+      "1. Tell me more about Amador\n"
+      "2. Map of Amador (Where are the classrooms and bathrooms)\n"
+      "3. Student Resources\n"
+      "Or feel free to write whatever: ")
