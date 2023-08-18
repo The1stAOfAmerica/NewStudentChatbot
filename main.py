e538461edb6f2e38268940162f47c59cc737e941
amitkrishna <akrocks1248@gmail.com>
2023-08-18 10:58:57 -0700

    wooot woot

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import os
+
+
 print("Hello New Student! What would you like to know about?\n")
-answer = input("Here are some suggested Questions:\n"
-      "1. Tell me more about Amador\n"
-      "2. Map of Amador (Where are the classrooms and bathrooms)\n"
-      "3. Student Resources\n"
-      "Or feel free to write whatever: ")
+
+answer = ""
+while answer != "exit":
+      answer = input("Here are some suggested Questions:\n"
+            "1. Tell me more about Amador\n"
+            "2. Map of Amador (Where are the classrooms and bathrooms)\n"
+            "3. Student Resources\n"
+                     "'exit' to exit\n"
+            "Or feel free to write whatever: ")
+
+      if 'bathroom' in answer:
+            print ("The bathroom is in the builing")
+      else:
+            print ("I'm sorry, I don't understand. Please check your spelling and retry")
+
